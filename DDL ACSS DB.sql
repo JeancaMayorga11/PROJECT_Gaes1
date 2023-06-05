5b7a90091ee9c1cf388a2d2ae9a14c255c3810f2
Jeank <65027041+JeancaMayorga11@users.noreply.github.com>
2023-06-05 10:54:43 -0500

    add FK TO DDL ACSS DB.sql

--- DDL ACSS DB.sql
+++ DDL ACSS DB.sql
@@ -6,6 +6,16 @@ CREATE TABLE Habilitado(
 ID_habilitado bigint(12) not null primary key,
 Nombre varchar(10) not null);
 
+CREATE TABLE Rol(
+ID_rol bigint(12) not null primary key,
+Nom_rol varchar(15) not null,
+fk_ID_usuario bigint(12) not null,
+fk_ID_habilitado bigint(12) not null,
+fk_ID_perfil bigint(12) not null,
+foreign key(fk_ID_usuario) references Usuario (ID_usuario),
+foreign key(fk_ID_perfil) references Perfil (ID_perfil),
+foreign key(fk_ID_habilitado) references Habilitado (ID_habilitado));
+
 
 CREATE TABLE Usuario(
 ID_usuario bigint (12) not null primary key,
@@ -15,28 +25,21 @@ Nombres varchar(30) not null,
 Apellidos varchar (30) not null,
 Telefono bigint (15) not null,
 Tipoidentificacion  varchar (15) not null,
+fk_ID_rol bigint(12) not null,
 fk_ID_habilitado bigint (12) not null,
-foreign key(fk_ID_habilitado) references Habilitado(ID_habilitado));
+foreign key(fk_ID_rol) references Rol (ID_rol),
+foreign key(fk_ID_habilitado) references Habilitado (ID_habilitado));
 
 
 CREATE TABLE Perfil(
 ID_perfil bigint(12) not null primary key,
 Nombre varchar(15) not null,
+fk_ID_rol bigint(12) not null,
 fk_ID_usuario bigint(12) not null,
 fk_ID_habilitado bigint(12)not null,
-foreign key(fk_ID_usuario) references Usuario(ID_usuario),
-foreign key(fk_ID_habilitado) references Habilitado(ID_habilitado));
-
-
-CREATE TABLE Rol(
-ID_rol bigint(12) not null primary key,
-Nom_rol varchar(15) not null,
-fk_ID_usuario bigint(12) not null,
-fk_ID_habilitado bigint(12) not null,
-fk_ID_perfil bigint(12) not null,
-foreign key(fk_ID_usuario) references Usuario(ID_usuario),
-foreign key(fk_ID_perfil) references Perfil(ID_perfil),
-foreign key(fk_ID_habilitado) references Habilitado(ID_habilitado));
+foreign key(fk_ID_rol) references Rol (ID_rol),
+foreign key(fk_ID_usuario) references Usuario (ID_usuario),
+foreign key(fk_ID_habilitado) references Habilitado (ID_habilitado));
 
 
 CREATE TABLE Cita(
@@ -48,8 +51,8 @@ HoraVisita time(6) null,
 TipoServicio varchar(25) not null,
 fk_ID_perfil bigint (12),
 fk_ID_habilitado bigint(12),
-foreign key(fk_ID_perfil) references Perfil(ID_perfil),
-foreign key(fk_ID_habilitado) references Habilitado(ID_habilitado));
+foreign key(fk_ID_perfil) references Perfil (ID_perfil),
+foreign key(fk_ID_habilitado) references Habilitado (ID_habilitado));
 
 
 CREATE TABLE Equipo(
@@ -73,11 +76,21 @@ fk_ID_perfil bigint(12),
 fk_ID_cita bigint(12),
 fk_ID_Numero_referencia bigint(12),
 fk_ID_habilitado bigint(12),
-Foreign key(fk_ID_Perfil) references perfil (ID_perfil),
+Foreign key(fk_ID_perfil) references perfil (ID_perfil),
 Foreign key(fk_ID_cita) references cita (ID_cita),
 Foreign key(fk_ID_numero_referencia) references equipo (Numero_referencia),
-foreign key(fk_ID_habilitado) references Habilitado(ID_habilitado));
+foreign key(fk_ID_habilitado) references Habilitado (ID_habilitado));
 
+
+CREATE TABLE Elemento(
+COD_elemento bigint (12) not null primary key,
+Nombre varchar (12),
+Cantidad int(4),
+Precio Float(12) not null,
+fk_ID_perfil bigint(10),
+fk_ID_habilitado bigint(12),
+foreign key (fk_ID_perfil) references Perfil (ID_perfil),
+foreign key (fk_ID_habilitado) references Habilitado (ID_habilitado));
 
 CREATE TABLE Inventario(
 ID_inventario bigint (12) not null primary key,
@@ -89,22 +102,11 @@ Saldo Float (12) not null,
 ValorEntrada float(12) not null,
 ValorSalida float(12),
 fk_ID_perfil bigint(10),
+fk_COD_elemento bigint (12) not null,
 fk_ID_habilitado bigint(12),
 foreign key (fk_ID_perfil) references Perfil (ID_perfil),
+foreign key (fk_COD_elemento) references Elemento (COD_elemento),
 foreign key (fk_ID_habilitado) references Habilitado (ID_habilitado));
-
-
-CREATE TABLE Elemento(
-COD_elemento bigint (12) not null primary key,
-Nombre varchar (12),
-Cantidad int(4),
-Precio Float(12) not null,
-fk_ID_perfil bigint(10),
-fk_ID_habilitado bigint(12),
-fk_ID_inventario bigint(12),
-foreign key (fk_ID_perfil) references Perfil (ID_perfil),
-foreign key (fk_ID_habilitado) references Habilitado (ID_habilitado),
-foreign key (fk_ID_inventario) references Inventario (ID_inventario));
 
 
 CREATE TABLE Categoria(
@@ -113,9 +115,9 @@ NombreCategoria varchar(15) not null,
 fk_ID_perfil bigint(12) , 
 fk_COD_elemento bigint(12) ,
 fk_ID_habilitado bigint(12),
-foreign key(fk_ID_perfil) references Perfil(ID_perfil), 
-foreign key(fk_COD_elemento) references Elemento(COD_elemento),
-foreign key(fk_ID_habilitado) references Habilitado(ID_habilitado));
+foreign key(fk_ID_perfil) references Perfil (ID_perfil), 
+foreign key(fk_COD_elemento) references Elemento (COD_elemento),
+foreign key(fk_ID_habilitado) references Habilitado (ID_habilitado));
 
 
 CREATE TABLE Entrada(
@@ -123,8 +125,8 @@ ID_Entrada bigint(12) not null primary key,
 FechaEntrada datetime(6) null, 
 fk_ID_perfil bigint(10) not null, 
 fk_COD_elemento bigint(10) not null, 
-foreign key(fk_ID_perfil) references Perfil(ID_perfil), 
-foreign key(fk_COD_elemento) references Elemento(COD_elemento));
+foreign key(fk_ID_perfil) references Perfil (ID_perfil), 
+foreign key(fk_COD_elemento) references Elemento (COD_elemento));
 
 
 CREATE TABLE Salida(
@@ -132,8 +134,8 @@ ID_Salida bigint(12) not null primary key,
 FechaSalida datetime(6) null, 
 fk_ID_perfil bigint(10) not null, 
 fk_COD_elemento bigint(10) not null,
-foreign key(fk_ID_perfil) references Perfil(ID_Perfil), 
-foreign key(fk_COD_elemento) references Elemento(COD_elemento));
+foreign key(fk_ID_perfil) references Perfil (ID_perfil), 
+foreign key(fk_COD_elemento) references Elemento (COD_elemento));
 
 
 
